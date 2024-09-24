82d07eb964fc6a4b89b8631db354a2ec89b392d5
Richard Dodd <richard@rayon.so>
2024-09-24 12:17:03 +0100

    fix warning

--- src/lib.rs
+++ src/lib.rs
@@ -69,7 +69,7 @@ struct DependencyNode {
 }
 
 impl fmt::Debug for DependencyNode {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "DependencyNode(\"{:?}\")", self.filename)
     }
 }
